2c737a9e4d2e34ee2d4f9b3e524990c9bf48bec7
MrGallego <44037673+MrGallego@users.noreply.github.com>
2023-06-03 02:27:21 -0500

    limpieza

--- ServiceClima/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace ServiceClima.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
